880494d1fb8f89e7f3ff760724c2cca46e397fcc
Krishna <kdwivedi343@gmail.com>
2024-05-03 22:27:50 +0530

    Added env

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async (DB_URL) => {
     try {
-        mongoose.connect("mongodb+srv://xceedesigns:ee1bQcTcuGYj1rCj@xceedesigns.4qdrpfu.mongodb.net/Xceed");
+        mongoose.connect(process.env.MONGODB_URI);
         console.log("Database connected successfully");
     } catch (error) {
         console.log("Error connecting to database", error.message);
