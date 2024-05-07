12d219ec551c39f712ad7f01e752b81758208001
HxrshB <bharsh.shg@gmail.com>
2024-05-07 13:51:45 +0530

    Indulged authentication in fetch request.

--- routes/contact.js
+++ routes/contact.js
@@ -10,64 +10,58 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 router.use(cors());
 
-
 // Saves data into database
-router.post("/", [query('email').isEmail(), query('name').isEmpty()], async(req, res) => {
+router.post(
+  "/",
+  [query("email").isEmail(), query("name").isEmpty()],
+  async (req, res) => {
     const result = validationResult(req);
     if (result.isEmpty()) {
-        return res.send({ errors: result.array() });
+      return res.send({ errors: result.array() });
     }
     try {
-        const { name, email, message } = req.body;
-        const newUser = new User({
-            name: name,
-            email: email,
-            message: message
-        });
+      const { name, email, message } = req.body;
+      const newUser = new User({
+        name: name,
+        email: email,
+        message: message,
+      });
 
-        try {
-            const result = await newUser.save();
-            res.send(result);
-        } catch (error) {
-            res.send(error.message);
-        }
-        
-
-    } catch (error) {
+      try {
+        const result = await newUser.save();
+        res.send(result);
+      } catch (error) {
         res.send(error.message);
+      }
+    } catch (error) {
+      res.send(error.message);
     }
-
-});
-
+  }
+);
 
 // Fetch users
-router.get("/fetch", async(req, res) => {
-    try {
-        const users = await User.find();
-        res.send(users);
-    } catch (error) {
-        res.send(error.message);
+router.get("/fetch", async (req, res) => {
+  try {
+    if (req.body.adminAuth != "@Admin123") {
+      res.send("Admin Not Authenticated");
+    } else {
+      const users = await User.find();
+      res.send(users);
     }
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 // Delete User Contact
-router.delete("/delete/:id", async(req, res) => {
-    try {
-        const id = req.params.id;
-        const user = await User.findByIdAndDelete(id);
-        res.send(user);
-    } catch (error) {
-        res.send(error.message);
-    }
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findByIdAndDelete(id);
+    res.send(user);
+  } catch (error) {
+    res.send(error.message);
+  }
 });
-
-// Authenticate Admin
-router.post("/admin", async(req, res) => {
-    if(req.body.adminAuth === "@Admin123") {
-        res.send("Admin Authenticated");
-    }
-    else res.send("Admin Not Authenticated");
-});
-
 
 module.exports = router;
